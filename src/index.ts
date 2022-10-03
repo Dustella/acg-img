f0cd94382b03bd4b438eea3e34997cde669cc139
Dustella Chan <fdnoaivj@outlook.com>
2022-10-03 14:48:43 +0800

    ⚡️ add cors

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,42 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import data from '../data.json'
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+import data from "../data.json";
 
 export interface Env {
+  __STATIC_CONTENT: string;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const length = data.length
-		const randomIndex = Math.floor(Math.random() * length)
-		return new Response(JSON.stringify({code:0,url:data[randomIndex],message:"OK"}));
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    if (request.url.includes("api")) {
+      const length = data.length;
+      const randomIndex = Math.floor(Math.random() * length);
+      const resp = new Response(
+        JSON.stringify({ code: 0, url: data[randomIndex], message: "OK" })
+      );
+      // Set CORS headers
+      resp.headers.set("Access-Control-Allow-Origin", "*");
+      // Append to/Add Vary header so browser will cache response correctly
+      resp.headers.append("Vary", "Origin");
+      return resp;
+    }
+    return await getAssetFromKV(
+      {
+        request,
+        waitUntil(promise) {
+          ctx.waitUntil(promise);
+        },
+      },
+      {
+        ASSET_NAMESPACE: env.__STATIC_CONTENT,
+        // ASSET_MANIFEST: assetManifest,
+      }
+    );
+  },
 };
