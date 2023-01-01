3ea33fa725c6d194b0e9e30fb2d6640cc4bd6857
Dustella <fdnoaivj@outlook.com>
2023-01-01 13:53:07 +0800

    ⚡️

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 import manifestJSON from '__STATIC_CONTENT_MANIFEST'
 import data from "../data.json";
+import cats_url from "../cats_url.json"
 
 let manifest = JSON.parse(manifestJSON)
 
@@ -24,6 +25,19 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
+    if (request.url.includes("cat")) {
+      const length = cats_url.length;
+      const randomIndex = Math.floor(Math.random() * length);
+      const resp = new Response(
+        JSON.stringify({ code: 0, url: cats_url[randomIndex], message: "OK" })
+      );
+      // Set CORS headers
+      resp.headers.set("Access-Control-Allow-Origin", "*");
+      // Append to/Add Vary header so browser will cache response correctly
+      resp.headers.append("Vary", "Origin");
+      return resp;
+    }
+
     if (request.url.includes("api")) {
       const length = data.length;
       const randomIndex = Math.floor(Math.random() * length);
